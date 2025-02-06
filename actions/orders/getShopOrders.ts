9f13a48c73d306e5828cddbdef96e726256fede1
zuhair khan <zuhairkhan5134@gmail.com>
2025-02-06 18:29:37 +0530

    new update

--- actions/orders/getShopOrders.ts
+++ actions/orders/getShopOrders.ts
@@ -1,37 +1,45 @@
 'use server'
 
-import { currentUser, type User } from '@clerk/nextjs/server'
 import prisma from '../../lib/prismaDb'
+import { createClerkClient } from '@clerk/backend';
 
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
 
-export const getShopOrders = async({sellerId} : {sellerId:string})=>{
-    try{
-        const orders:any = await prisma.orders.findMany({
-            where:{
-                Prompts:{
+export const getShopOrders = async ({ sellerId }: { sellerId: string }) => {
+    try {
+        const orders: any = await prisma.orders.findMany({
+            where: {
+                Prompts: {
                     sellerId
                 }
             },
-            include:{
+            include: {
                 Prompts: true,
             }
-        })
+        });
 
-        for(const order of orders){
+        for (const order of orders) {
             const userId = order?.userId;
-            console.log("IUser" , userId) ;
-            if(userId){
-                const user = await users.getUser(userId) ;
-                order.user = user ;
+            console.log("IUser", userId);
+            
+            if (userId) {
+                const user = await clerkClient.users.getUser(userId);
+             //console.log("email data" ,user.emailAddresses?.[0]?.emailAddress )
+                // Convert Clerk user object to plain JSON
+                order.user = {
+                    id: user.id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.emailAddresses?.[0]?.emailAddress || null,
+                    imageUrl: user.imageUrl
+                };
             }
         }
 
-        //console.log("orders" , orders)
+        return orders;
 
-        return orders
-
-
-    }catch(error){
-        console.log("Shop orders error" , error);
+    } catch (error) {
+        console.log("Shop orders error", error);
+        return [];
     }
-}
+};
